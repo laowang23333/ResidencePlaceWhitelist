1bebec0b8c3315c0e4722b14bae1fb52c9f2998a
laowang23333 <59322147+laowang23333@users.noreply.github.com>
2026-09-11 02:42:21 +0800

    Update ResidencePlaceWhitelist.java

--- src/main/java/com/example/residencewhitelist/ResidencePlaceWhitelist.java
+++ src/main/java/com/example/residencewhitelist/ResidencePlaceWhitelist.java
@@ -1,8 +1,12 @@
 package com.example.residencewhitelist;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ResidencePlaceWhitelist extends JavaPlugin {
+public class ResidencePlaceWhitelist extends JavaPlugin implements CommandExecutor {
 
     private BlockPlaceListener listener;
 
@@ -11,6 +15,10 @@ public class ResidencePlaceWhitelist extends JavaPlugin {
         saveDefaultConfig();
         listener = new BlockPlaceListener(this);
         getServer().getPluginManager().registerEvents(listener, this);
+        
+        // 注册指令
+        getCommand("rpw").setExecutor(this);
+        
         getLogger().info("ResidencePlaceWhitelist 已启用！");
     }
 
@@ -21,5 +29,20 @@ public class ResidencePlaceWhitelist extends JavaPlugin {
 
     public BlockPlaceListener getListener() {
         return listener;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("rpw")) {
+            if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+                // 重载配置
+                listener.reloadWhitelist();
+                sender.sendMessage(ChatColor.GREEN + "[RPW] 白名单配置已成功重载！");
+                return true;
+            }
+            sender.sendMessage(ChatColor.RED + "用法: /rpw reload");
+            return true;
+        }
+        return false;
     }
 }
